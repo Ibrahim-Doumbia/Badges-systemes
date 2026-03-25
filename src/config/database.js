ee575fdd832d440bd1a7d30a11639e9fa2576642
Ibrahim-Doumbia <ibrahim68doumbia@gmail.com>
2026-03-25 23:34:16 +0100

    fix: ajouter SSL support dans  Railway PostgreSQL

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,27 @@
-const {Sequelize} = require("sequelize")
-require("dotenv").config()
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host : process.env.DB_HOST,
-        dialect : 'postgres',
-        logging : true
-    }
-)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        logging: false,
+      }
+    );
 
-module.exports = sequelize
-
+module.exports = sequelize;
