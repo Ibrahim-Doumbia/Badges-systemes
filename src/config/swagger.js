e744fc50fc42a41719b8cf479142ee6b2b0e1ade
Ibrahim-Doumbia <ibrahim68doumbia@gmail.com>
2026-03-26 00:27:36 +0100

    modification de swagger.js a cause de railway

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,8 +10,10 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api",
-        description: "Serveur de développement",
+        // url: "http://localhost:3000/api",
+        // description: "Serveur de développement",
+        url: "/api",
+        description: "Serveur production",
       },
     ],
     components: {
